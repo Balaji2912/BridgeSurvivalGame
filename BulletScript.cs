4bd01024fa2b8ce0b0b2eec102c226da43fae5c7
Balaji2912 <77846483+Balaji2912@users.noreply.github.com>
2021-01-22 21:20:38 +0530

    Add files via upload

---
+++ BulletScript.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bullet : MonoBehaviour
+{
+    //public float health;
+    public float damage = 20f;
+    public Transform BulletSpawnPoint;
+    //public float bulletSpeed;
+    public GameObject BulletPrefab;
+    public float bulletImpactForce = 10f;
+    //public Animator run;
+    public ParticleSystem ParticleImpactEF;
+    public Vector3 spawnHere;
+    
+    
+    void update()
+    {
+        //BulletSpawn.GetComponent<Transform>().position;
+        
+       
+    }
+    void start()
+    {
+        //run = GetComponent<Animator> ();
+        //GetComponent<Animator>().enabled = false;
+        //run.enabled = true;
+    }
+    void ImpactEffect()
+    {
+        ParticleImpactEF.transform.parent = null;
+        ParticleImpactEF.Play();
+        Destroy(ParticleImpactEF, 2f);
+    }
+
+    private void OnTriggerEnter(Collider target)
+    {
+        ImpactEffect();
+        //Destroy(ImpactEffect, 2f);
+        if (target.transform.tag == "Enemy")
+        {
+            
+            //Instantiate(ParticleImpactEF, target.transform.position, Quaternion.identity);
+            //target.GetComponent<Rigidbody>().AddForce(BulletSpawnPoint.forward * bulletSpeed, ForceMode.Impulse);
+            target.GetComponent<Rigidbody>().AddForce(BulletSpawnPoint.forward * bulletImpactForce, ForceMode.Impulse);
+            //target.GetComponent<Enemy>().health -= damage;
+            Debug.Log("hit" + target.name + "!");
+            Destroy(BulletPrefab);
+            //GetComponent<Enemy>();
+            EnemyDie enemy = target.transform.GetComponent<EnemyDie>();
+
+    
+            
+            if(enemy != null)
+            {
+                    //target.GetComponent<Rigidbody>().AddForce(BulletSpawnPoint.forward * bulletImpactForce, ForceMode.Impulse);
+                //impact();
+                Collider[] colliders = Physics.OverlapSphere(target.transform.position,50f);
+                foreach (Collider rb in colliders) 
+                {
+                    Rigidbody rigidbody = rb.GetComponent<Rigidbody>();
+                    if(rigidbody !=null)
+                    {
+                            //EnemyDie enemy = target.transform.GetComponent<EnemyDie>();
+                        target.GetComponent<Rigidbody>().GetComponent<Rigidbody>().AddExplosionForce(500f, target.transform.position, 50f);
+                        //enemy.die();
+                    }
+                }  
+                enemy.die();
+            }
+        }
+        if (target.transform.tag == "Ground")
+        {
+            Debug.Log("HitGround");
+            //spawnHere = new Vector3 (Collider.transform.position)
+            //Instantiate(ParticleImpactEF, spawnHere, target.transform.rotation);
+            //Instantiate(ParticleImpactEF, target.transform.position, target.transform.rotation);
+            //Destroy(BulletPrefab);
+            //Instantiate(ParticleImpactEF, target.transform.position, Quaternion.identity);
+            
+        }        
+        
+
+        else
+        {
+            
+            Debug.Log("NOTHit");
+        }
+            
+    }
+    /*void impact()
+    {
+        //Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+        Collider[] colliders = Physics.OverlapSphere(BulletSpawnPoint.position ,50f);
+        foreach (Collider rb in colliders) 
+        {
+            Rigidbody rigidbody = rb.GetComponent<Rigidbody>();
+            if(rigidbody !=null)
+            {
+                //EnemyDie enemy = target.transform.GetComponent<EnemyDie>();
+                rigidbody.AddExplosionForce(500f, BulletSpawnPoint.position, 50f);
+            //enemy.die();
+            }
+        }  
+    }*/
+}
+
---
+++ PickUpController.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpController : MonoBehaviour
+{
+    public BulletGun gunScript;
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform player, GunHolder, fpsCam;
+
+    public float pickUpRange;
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+    public Button DropGun;
+    public Button pickGun;
+    public Button ShootButton;
+    public Animator animator;
+
+    //public BulletGun Shoot;
+    //public bool isShoot;
+    public void DropGunfunc()
+    {
+
+        //Drop if equipped and "Q" is pressed
+        if (equipped)
+        {
+            //isShoot = false;
+            Drop();
+            
+            //Add random rotation
+            
+            ShootButton.interactable = false;
+            animator.enabled = false;
+            
+            
+        }
+
+    }
+    public void pickGunfunc()
+    {
+        Vector3 distanceToPlayer = player.position - transform.position;
+        if (!equipped && distanceToPlayer.magnitude <= pickUpRange )
+        {
+            PickUp();
+            //isShoot = true;
+            ShootButton.interactable = true;
+            animator.enabled = true;
+        }
+
+
+    }
+
+    void Start()
+    {
+        //Setup
+        /*if (isShoot == true)
+        {
+            Shoot();
+        }*/
+        if (!equipped)
+        {
+            gunScript.enabled = false;
+            rb.isKinematic = false;
+            coll.isTrigger = false;
+            //BulletGun.GetComponent<isShoot>() = false;
+        }
+        if (equipped)
+        {
+            gunScript.enabled = true;
+            rb.isKinematic = true;
+            coll.isTrigger = true;
+            slotFull = true;
+        }
+    }
+
+    private void Update()
+    {
+        //Check if player is in range and "E" is pressed
+        /*if(transform.SetParent(null))
+        {
+            ShootButton.interactable = false;
+        }
+        else
+        {
+            ShootButton.interactable = true;
+        }*/        
+    }
+
+    private void PickUp()
+    {
+        equipped = true;
+        slotFull = true;
+
+        //Make weapon a child of the camera and move it to default position
+        transform.SetParent(GunHolder);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one;
+
+        //Make Rigidbody kinematic and BoxCollider a trigger
+        rb.isKinematic = true;
+        coll.isTrigger = true;
+
+        //Enable script
+        gunScript.enabled = true;
+    }
+
+    private void Drop()
+    {
+        equipped = false;
+        slotFull = false;
+
+        //Set parent to null
+        transform.SetParent(null);
+        ShootButton.interactable = true;
+
+        //Make Rigidbody not kinematic and BoxCollider normal
+        rb.isKinematic = false;
+        coll.isTrigger = false;
+
+        //Gun carries momentum of player
+        rb.velocity = player.GetComponent<Rigidbody>().velocity;
+
+        //AddForce
+        rb.AddForce(fpsCam.forward * dropForwardForce, ForceMode.Impulse);
+        rb.AddForce(fpsCam.up * dropUpwardForce, ForceMode.Impulse);
+        float random = Random.Range(-1f, 2f);
+        rb.AddTorque(new Vector3(random, random, random) * 10);
+
+        //Disable script
+        gunScript.enabled = false;
+    }
+}
