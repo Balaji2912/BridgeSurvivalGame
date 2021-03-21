6a1ef083d9ebb71f59605a8b67c6ee4a2b34c4d8
Balaji2912 <77846483+Balaji2912@users.noreply.github.com>
2021-03-21 10:54:41 +0530

    Update ProjectileGun.cs

--- ProjectileGun.cs
+++ ProjectileGun.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,30 +7,21 @@ public class BulletGun : MonoBehaviour
 {
     public GameObject BulletPrefab;
     public Transform BulletSpawnPoint;
-    public float bulletSpeed = 30;
-    public GameObject MuzzleFlash;
-    public float damage = 20f;
-    public GameObject Light;
-    public float BulletlifeTime;
-    public float LightlifeTime;
-    public bool lighton;
-    public Animator animator;
-    public Animation Recoil;
-    public float FireanimTime = 0.01f;
+    public float bulletSpeed = 30, damage = 20f, BulletlifeTime, LightlifeTime, FireanimTime = 0.01f, interactablebuttonTim = .5f;
+    //public GameObject MuzzleFlash;
+    public float maxBullet, reload = 1.5f, upwardForce, spread;//MuzzleFlashTime = 0.3f;
+    //public GameObject Light;
+    //public bool lighton;
+    public Animator animator, Recoil;
     public Button Bu;
-    public float interactablebuttonTim  = .5f;
-    public float MuzzleFlashTime = 0.3f;
-    public AudioSource shootsound;
-    public AudioSource ReloadSound;
-    public float maxBullet;
+    //public AudioSource shootsound, ReloadSound;
     private float currentBullet;
-    private bool isShoot = true;
-    private bool isReloading = false;
+    private bool isShoot = true, isReloading = false;
     public Text ammoDisplay;
-    public float reload = 1.5f;
-    public Button Dropbut;
+    //public Button Dropbut;
+    public Camera Maincamera;
 
-    
+
     public void ButtonFire()
     {
 
@@ -46,21 +37,21 @@ public class BulletGun : MonoBehaviour
         yield return new WaitForSeconds(interactablebuttonTim);
         Bu.interactable = true;
     }
-    
+
     void Start()
     {
-        
+
         isShoot = true;
         currentBullet = maxBullet;
         ammoDisplay.text = currentBullet.ToString();
     }
-    
-    
+
+
     IEnumerator FireAnimation()
     {
-        animator.SetBool("Fire",true);
+        animator.SetBool("Fire", true);
         yield return new WaitForSeconds(FireanimTime);
-        animator.SetBool("Fire",false);
+        animator.SetBool("Fire", false);
     }
 
     // Update is called once per frame
@@ -68,78 +59,98 @@ public class BulletGun : MonoBehaviour
     {
         if (currentBullet == 0)
         {
-            
+
             isShoot = false;
-            
+
             StartCoroutine(Reloadanim());
-            
+
             return;
         }
         if (currentBullet == 0)
         {
-            ReloadSound.Play();
+            //ReloadSound.Play();
         }
         if (isReloading == true)
         {
             return;
         }
-           
-          
+
+
     }
 
     IEnumerator Shoot()
     {
         ammoDisplay.text = currentBullet.ToString();
         isShoot = true;
-        MuzzleFlash.SetActive(true);
-        StartCoroutine(Muzzlefunction());
-        Light.SetActive(true);
+        Ray ray = Maincamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.GetPoint(75);
+
+        Vector3 directionWithoutSpread = targetPoint - BulletSpawnPoint.position;
+
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0);
+
+
+        //MuzzleFlash.SetActive(true);
+        //StartCoroutine(Muzzlefunction());
+        //Light.SetActive(true);
         StartCoroutine(FireAnimation());
         StartCoroutine(ButtonEnable());
-        shootsound.Play();
+        //shootsound.Play();
         Bu.interactable = true;
-        GameObject Bullet = Instantiate(BulletPrefab);
+        GameObject Bullet = Instantiate(BulletPrefab, BulletSpawnPoint.position, Quaternion.identity);
+
+        Bullet.transform.forward = directionWithSpread.normalized;
         Physics.IgnoreCollision(Bullet.GetComponent<Collider>(), BulletSpawnPoint.parent.GetComponent<Collider>());
         Bullet.transform.position = BulletSpawnPoint.position;
         Vector3 rotation = Bullet.transform.rotation.eulerAngles;
         Bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
-        Bullet.GetComponent<Rigidbody>().AddForce(BulletSpawnPoint.forward * bulletSpeed, ForceMode.Impulse);
+        Bullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * bulletSpeed, ForceMode.Impulse);
+        Bullet.GetComponent<Rigidbody>().AddForce(Maincamera.transform.up * upwardForce, ForceMode.Impulse);
         StartCoroutine(DestroyBulletAfterTime(Bullet, BulletlifeTime));
-        StartCoroutine(DestroyLightAfterTime(Light, LightlifeTime));
+        //StartCoroutine(DestroyLightAfterTime(Light, LightlifeTime));
         yield return new WaitForSeconds(FireanimTime);
-        currentBullet --;
+        currentBullet--;
         ammoDisplay.text = currentBullet.ToString();
 
-        
+
     }
 
     IEnumerator Reloadanim()
     {
-        
+
         isShoot = false;
         isReloading = true;
-        Dropbut.interactable = false;
+        //Dropbut.interactable = false;
         StartCoroutine(reloadf2());
         currentBullet = maxBullet;
         isReloading = false;
         yield return new WaitForSeconds(reload);
         isShoot = true;
-        Dropbut.interactable = true;
+        //Dropbut.interactable = true;
         ammoDisplay.text = currentBullet.ToString();
     }
     IEnumerator reloadf2()
     {
-        ReloadSound.Play();
-        animator.SetBool("Recoil",true);
+        //ReloadSound.Play();
+        animator.SetBool("Reload", true);
         yield return new WaitForSeconds(1f);
-        animator.SetBool("Recoil",false);
+        animator.SetBool("Reload", false);
     }
-    IEnumerator Muzzlefunction()
+    /*IEnumerator Muzzlefunction()
     {
-        
+
         yield return new WaitForSeconds(MuzzleFlashTime);
         MuzzleFlash.SetActive(false);
-    }
+    }*/
 
 
     private IEnumerator DestroyBulletAfterTime(GameObject Bullet, float delay)
@@ -147,16 +158,16 @@ public class BulletGun : MonoBehaviour
         yield return new WaitForSeconds(delay);
         Destroy(Bullet);
     }
-    private IEnumerator DestroyLightAfterTime(GameObject Light, float delay)
+    /*private IEnumerator DestroyLightAfterTime(GameObject Light, float delay)
     {
         yield return new WaitForSeconds(delay);
-        GameObject.Find ("Light") .SetActive (false);
-        
-    }
+        GameObject.Find("Light").SetActive(false);
+
+    }*/
     void Shootandanim()
     {
-        StartCoroutine(FireAnimation());
+        //StartCoroutine(FireAnimation());
         StartCoroutine(Shoot());
     }
-   
+
 }
